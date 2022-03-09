5c894e163fdf897df38e16917318fa5be96c72b4
ВундерВафля <64733647+AlexandrVino@users.noreply.github.com>
2022-03-09 08:22:34 +0500

    Update settings.py

--- FirstHomeTask/settings.py
+++ FirstHomeTask/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'about', 
+    'homepage',
+    'catalog',
+    'users',
+    'rating'
 ]
 
 MIDDLEWARE = [
