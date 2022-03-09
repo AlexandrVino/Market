7729996a9a65efceaea1e5fa90caee2172cb17d7
AlexandrVino <64733647+ded-masay@users.noreply.github.com>
2022-03-09 15:08:06 +0500

    Edit installed apps .3

--- FirstHomeTask/settings.py
+++ FirstHomeTask/settings.py
@@ -38,11 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'FirstHomeTask.about.apps.AboutConfig',
-    'FirstHomeTask.users.apps.UsersConfig',
-    'FirstHomeTask.homepage.apps.HomepageConfig',
-    'FirstHomeTask.rating.apps.RatingConfig',
-    'FirstHomeTask.catalog.apps.CatalogConfig',
+    'about.apps.AboutConfig',
+    'users.apps.UsersConfig',
+    'homepage.apps.HomepageConfig',
+    'rating.apps.RatingConfig',
+    'catalog.apps.CatalogConfig',
 
 ]
 
