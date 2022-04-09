14f2919897d080a2eaa0a3a5996f8a76d7e7896c
AlexandrVino <64733647+ded-masay@users.noreply.github.com>
2022-04-09 19:43:33 +0500

    optimize imports

--- about/views.py
+++ about/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
 from http import HTTPStatus
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
--- rating/admin.py
+++ rating/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from rating.models import Rating
 
 
