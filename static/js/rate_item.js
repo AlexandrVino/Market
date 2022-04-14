9c213ccad5c76d13f61e13e5434185834e49a716
AlexandrVino <64733647+ded-masay@users.noreply.github.com>
2022-04-14 18:30:22 +0500

    edit template and js

--- static/js/rate_item.js
+++ static/js/rate_item.js
@@ -2,11 +2,17 @@ window.onload = () => {
     let rateForm = document.getElementById('rate_form');
     let item_info = document.getElementById('item_info');
     let button = document.getElementById('set_rate')
+    let close_button = document.getElementById('close_button')
 
     rateForm.hidden = true
 
     button.onclick = () => {
         rateForm.hidden = !rateForm.hidden;
         item_info.hidden = !rateForm.hidden;
+    }
+
+    close_button.onclick = () => {
+        rateForm.hidden = true;
+        item_info.hidden = false;
     }
 }
